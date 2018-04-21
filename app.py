05229c66ebac6c24d060dc86f93fe39f6d5d7e1b
denniswanjiru <denniswanjiru71@gmail.com>
2018-04-21 05:59:00 +0300

    feat[auth]: Setting up user Authentication Routes - Created /signup and /login routes - Returned some dummy text on both

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+from employee import Employee
 
 app = Flask(__name__)
 
+employees = []
+
+
+@app.route('/')
+def root():
+    return redirect(url_for('signup'))
+
+
+@app.route('/signup')
+def signup():
+    return 'Sign Up'
+
+
+@app.route('/login')
+def login():
+    return 'Log in'
+
+
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
